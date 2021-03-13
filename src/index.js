ec459211460065170c579bb267923d1184b29007
Sergey Syleimanov <ssuleimanov.94@gmail.com>
2021-03-13 20:25:35 +0600

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = [],
+        result = str;
+    bracketsConfig.flat(0)
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets.push(bracketsConfig[i].join(''))
+    }
+    for (let i = 0; i <= str.length; i++) {
+        for (let n = 0; n <= brackets.length; n++) {
+            result = result.replace(brackets[n], '')
+        }
+    }
+    return result.length === 0 ? true : false;
+
 }
